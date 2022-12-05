b8711004c3c6b000524763734f1495ebf1dd1106
Yi-99 <112568320+Yi-99@users.noreply.github.com>
2022-12-05 11:25:48 -0700

    first

---
+++ build/precache-manifest.e3f9333104f2e621468ea323e2aaea9f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02577035e911a96ae8ba80fe146e596c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7635dba87ef88cad582",
+    "url": "/static/js/2.00825209.chunk.js"
+  },
+  {
+    "revision": "a2076da57c9dce2e06dc",
+    "url": "/static/js/main.a21b1dbf.chunk.js"
+  },
+  {
+    "revision": "0c50609397080d9123f0",
+    "url": "/static/js/runtime-main.dea3c3de.js"
+  }
+]);
